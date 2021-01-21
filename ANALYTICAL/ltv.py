7c89eccc703d2000831f9ca9cc6cd4b118551743
Bohdan-KL <bohdanklots@gmail.com>
2021-01-21 15:40:30 +0200

    Way of counting LTV with strict formula. However, it gives strange results.

--- ANALYTICAL/ltv.py
+++ ANALYTICAL/ltv.py
@@ -1,41 +1,62 @@
 import pandas as pd
+from datetime import datetime
 
-# ltv = [1] + [2] + [3] + [4] + [5] =
-# = d * c + d * c * 1_to_2 + d * c * 1_to_2 * 2_to_3 + d * c * 1_to_2 * 2_to_3 * 3_to_4 +
-# + d * c * 1_to_2 * 2_to_3 * 3_to_4 * 4_to_5 = 
-# = d * c * (1 + 1_to_2 + 1_to_2 * 2_to_3 + 1_to_2 * 2_to_3 * 3_to_4 + 1_to_2 * 2_to_3 * 3_to_4 * 4_to_5)
-# (we count ltv for one client)
-# c = 1, as there is one opportunity to take free trial
-# d = price per one perchase
-# 1_to_2, 2_to_3, 3_to_4, 4_to_5 = 1 or 0, 1 - if subscibes, 1 as we count only for one subsciber, so
-# he/she can buy one license per one week;
-# 0 - if he doesn't subscibe.
-# resulting ltv formula = d * p, p = count of purchases. <----
-# [1] = dev proceeds(d) * conversion to trial(c) 
-# [2] = [1] * 1_to_2_purchase
-# [3] = [2] * 2_to_3_purchase
-# [4] = ...
+pd.options.display.max_columns = 20
 
 
-def count_lvt(price, conversions, is_have_trial=True):
+def days_between(d1, d2):
+    d1 = datetime.strptime(d1, "%Y-%m-%d")
+    d2 = datetime.strptime(d2, "%Y-%m-%d")
+    return abs((d2 - d1).days)
+
+
+def how_many_conversion(week):
+    return len(list(filter(lambda x: x >= week, all_conversions_list)))
+
+
+def count_lvt(price, conversions, is_apple=True, is_have_trial=True):
+    k = 1
     if is_have_trial:
         conversions -= 1
-    ltv = price * conversions
+    if is_apple:
+        k = 0.7
+    ltv = k * price * conversions
     return ltv
 
 
-DATASET = pd.read_csv('DataScience/data_analytics.csv')
+DATASET = pd.read_csv('data_analytics.csv')
+# print(DATASET)
 PRICE_PER_PURCHASE = 9.99
 
 subscriber_id_col = list(DATASET['Subscriber ID'])
+
 list_of_unique_clients_id = list(set(subscriber_id_col))
 list_of_count_of_conversions = \
     [subscriber_id_col.count(x) for x in list_of_unique_clients_id]
 dict_of_count_of_conversions = \
     dict(zip(list_of_unique_clients_id, list_of_count_of_conversions))
 
-# Pseudo code
-create new data set for writing ltv and other info.
+date_dict = {}
+for id in list_of_unique_clients_id:
+    date_dict[id] = list((DATASET[lambda x: x['Subscriber ID'] == id])['Event Date'])
 
-add to new data set dict_of_count_of_conversions
-add to new data set values counted with count_lvt() function
+all_conversions_list = []
+for id in date_dict:
+    if len(date_dict[id]) - 1 != 0:
+        all_conversions_list.append(len(date_dict[id]) - 1)
+
+_0to_1 = how_many_conversion(1)
+_1to_2 = how_many_conversion(2)
+_2to_3 = how_many_conversion(3)
+_3to_4 = how_many_conversion(4)
+_4to_5 = how_many_conversion(5)
+print(_0to_1, _1to_2, _2to_3, _3to_4, _4to_5)
+a1 = 9.99 * 0.7 * _0to_1
+a2 = a1 * _1to_2
+a3 = a2 * _2to_3
+a4 = a3 * _3to_4
+a5 = a4 * _4to_5
+print(a1+a2+a3+a4+a5)
+print()
+print(f'proceeds is {(_0to_1 + _1to_2 + _2to_3 + _3to_4 + _4to_5)*9.99*0.7}')
+print(f'LTV for all users is {(9.99 * 0.7) * _0to_1 * (1 + _1to_2 + _1to_2 * _2to_3 + _1to_2 * _2to_3 * _3to_4 + _1to_2 * _2to_3 * _3to_4 * _4to_5 )}')
